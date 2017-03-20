8411bc9f9f8b052fd3ea6bf528f10377deff01c2
rachelandrews <rachelandrews95@yahoo.com>
2017-03-20 12:01:27 -0500

    Login and Registration VC working with some features

--- RunBevoRun/RunBevoRun/LoginViewController.swift
+++ RunBevoRun/RunBevoRun/LoginViewController.swift
@@ -9,25 +9,56 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
     @IBOutlet weak var password: UITextField!
-
     @IBOutlet weak var name: UITextField!
     
-    @IBOutlet weak var enter: UIButton!
-    
-    @IBOutlet weak var register: UIButton!
+    @IBOutlet weak var warningLbl: UILabel!
     
     var user: String = ""
+    var loginSuccess: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        user = name.text!
-
+        
     }
+    
 
+    @IBAction func login(_ sender: Any) {
+        if (name.text! == ""){
+            warningLbl.text = "Fill In Username"
+            return
+        }
+        loginSuccess = true
+//        let username = name.text!
+//        let pwd = password.text!
+//        
+//        let accounts = UserDefaults.standard
+//        
+//        let userStored = String(describing: accounts.value(forKey: "Username"))
+//        let userPwdStored = String(describing: accounts.value(forKey: "Password"))
+//        
+//        if(userStored == username && userPwdStored == pwd){
+//            accounts.set(true, forKey: "isUserLoggedIn")
+//            accounts.synchronize()
+//            self.dismiss(animated: true, completion: nil)
+//        }
+        
+    }
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
+        if let ident = identifier {
+            if ident == "loginToGame" {
+                if loginSuccess != true {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
@@ -38,6 +69,11 @@ class LoginViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        //TODO pass in username
+//        if (segue.identifier == "goToGame"){
+//
+//        }
     }
 
 }
--- RunBevoRun/RunBevoRun/RegisterViewController.swift
+++ RunBevoRun/RunBevoRun/RegisterViewController.swift
@@ -13,19 +13,30 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var name: UITextField!
     
     @IBOutlet weak var loginBack: UIButton!
-    @IBOutlet weak var register: UIButton!
+
     @IBOutlet weak var rePassword: UITextField!
     @IBOutlet weak var password: UITextField!
+    
+    var alertController:UIAlertController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+    }
+    
+    @IBAction func register(_ sender: Any) {
         let username = name.text
         let pwd = password.text
         let rePwd = rePassword.text
         
-        if (username == ""){
+        //Check if information is complete
+        if (username?.isEmpty)!{
             displayAlert(userMessage: "Username is empty")
+            return
+        }
+        if((pwd?.isEmpty)! || (rePwd?.isEmpty)!){
+            displayAlert(userMessage: "Password is empty")
             return
         }
         
@@ -33,12 +44,35 @@ class RegisterViewController: UIViewController {
             displayAlert(userMessage: "Passwords don't match")
             return
         }
+        //Add to data
+        let accounts = UserDefaults.standard
+        accounts.setValue(username, forKey:"Username")
+        accounts.setValue(pwd, forKey:"Password")
+        accounts.synchronize()
         
+        //Everything complete
+        //TODO have OK button go back to login?
+        self.alertController = UIAlertController(title: "Alert", message: "Successfully Registered", preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title:"Ok", style: UIAlertActionStyle.default, handler:nil)
+        
+        self.alertController!.addAction(okAction)
+        
+        self.present(self.alertController!, animated: true, completion:nil)
+       
     }
     
+    
     func displayAlert(userMessage:String){
-        //FIX
-       // var alert = UIAlertController(nibName: "Alert", bundle: userMessage)
+        
+        self.alertController = UIAlertController(title: "Error", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title:"Ok", style: UIAlertActionStyle.default, handler:nil)
+        
+        self.alertController!.addAction(okAction)
+        
+        self.present(self.alertController!, animated: true, completion:nil)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,8 +85,8 @@ class RegisterViewController: UIViewController {
 //     MARK: - Navigation
 //
 //     In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//    }
 
 }
