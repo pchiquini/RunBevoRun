9411e2ab3aeb321314f87a74e451210d7154f0b2
rachelandrews <rachelandrews95@yahoo.com>
2017-04-12 18:41:14 -0500

    adding UserInfo

---
+++ RunBevoRun/RunBevoRun/UserInfo.swift
@@ -0,0 +1,17 @@
+//
+//  Score.swift
+//  RunBevoRun
+//
+//  Created by Rachel Andrews on 4/12/17.
+//  Copyright © 2017 Chiquini. All rights reserved.
+//
+
+import Foundation
+
+class Score{
+    static let shared = Score()
+    var score = 0
+    
+    //when calling Score.shared.var
+    //if score is static, can access directly
+}
