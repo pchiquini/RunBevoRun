89e5a9d4ba7e5f017ccf37982568a023483487da
rachelandrews <rachelandrews95@yahoo.com>
2017-04-16 18:19:14 -0500

    Go directly from registration to login from alert

--- RunBevoRun/RunBevoRun/RegisterViewController.swift
+++ RunBevoRun/RunBevoRun/RegisterViewController.swift
@@ -49,14 +49,12 @@ class RegisterViewController: UIViewController {
         saveUser(username: username!, password: pwd!)
         
         
-        //Everything complete
-        //TODO have OK button go back to login?
+        //Successfully register and go back to login
         self.alertController = UIAlertController(title: "Sucess!", message: "Successfully Registered", preferredStyle: UIAlertControllerStyle.alert)
-        let okAction = UIAlertAction(title:"Ok", style: .default){ okAction -> Void in
-            let viewControllerYouWantToPresent = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
-            self.present(LoginViewController!, animated: true, completion: nil)
-        }
-        
+        let okAction = UIAlertAction(title:"Ok", style: .default) { (action) -> Void in
+            let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+            self.present(loginVC!, animated: true, completion: nil) }
+
         self.alertController!.addAction(okAction)
         
         self.present(self.alertController!, animated: true, completion:nil)
