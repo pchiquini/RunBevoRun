43bb8e1e7bfdf36aead477536b6f3af0d2a1c294
rachelandrews <rachelandrews95@yahoo.com>
2017-04-28 15:41:25 -0500

    merge conflict

--- RunBevoRun/RunBevoRun/Leaderboard.swift
+++ RunBevoRun/RunBevoRun/Leaderboard.swift
@@ -11,12 +11,21 @@ import SpriteKit
 import CoreData
 
 class Leaderboard: SKScene {
-
+    /*** Score Array ***/
+    var scores = [NSManagedObject]()
+    
     /*** Buttons On Scene ***/
     var mainMenuButton:SKSpriteNode!
     
     /** Scoring **/
-    var scoreLabel: SKLabelNode?
+    var player1:    SKLabelNode?
+    var player2:    SKLabelNode?
+    var player3:    SKLabelNode?
+    var player4:    SKLabelNode?
+    var score1:     SKLabelNode?
+    var score2:     SKLabelNode?
+    var score3:     SKLabelNode?
+    var score4:     SKLabelNode?
     
     /*** Starting Point ***/
     override func didMove(to view: SKView) {
@@ -31,14 +40,43 @@ class Leaderboard: SKScene {
     
     func createScene() {
         
+        
+        
         /** Displaying Score From GameScene **/
-        scoreLabel = self.childNode(withName: "scoreLabel") as! SKLabelNode?
-        scoreLabel!.text = String(UserInfo.shared.score)
+        score1 = self.childNode(withName: "score1") as! SKLabelNode?
+        score1!.text = String(UserInfo.shared.score)
         
         /** Adding Buttons to the GameScene **/
         mainMenuButton = self.childNode(withName: "mainMenuButton") as! SKSpriteNode
         
         //TODO add score to CoreData here?
+        
+        loadData()
+    }
+    
+    fileprivate func loadData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Scores")
+        
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        if let results = fetchedResults {
+            scores = results
+        } else {
+            print("Could not fetch")
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
